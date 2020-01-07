8d81aa4285a5a4f90f482ce57ac4d8a9d7d98f87
CmdrNorthpaw <thetuxkid@gmail.com>
2020-01-07 16:12:23 +0000

    Added find functionality

--- bot-main.py
+++ bot-main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from googlesearch import search
 import os
 
 bot = commands.Bot(command_prefix='xkcd ')
@@ -14,6 +15,12 @@ async def fetch(ctx, arg):
         await ctx.send('https://xkcd.com')
     else:
         await ctx.send('https://xkcd.com/%s' % str(arg))
+
+@bot.command()
+async def find(ctx, arg):
+    ctx.send('`Searching for xkcd comic...`')
+    queryResult = search(arg, tld=com, num=1 stop=1, pause=2))
+    ctx.send(queryResult)
 
 
 bot.run('Pretend this is a token')
