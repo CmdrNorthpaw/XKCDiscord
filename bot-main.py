2ce2947693ef17680ba4f060b33681d0870c5dac
CmdrNorthpaw <thetuxkid@gmail.com>
2020-01-07 17:14:27 +0000

    Added comments

--- bot-main.py
+++ bot-main.py
@@ -1,3 +1,4 @@
+# Imports the modules for operation and gets the key from the environment
 import discord
 from discord.ext import commands
 from googlesearch import search
@@ -6,10 +7,12 @@ key = environ.get('discordKey')
 
 bot = commands.Bot(command_prefix='xkcd ')
 
+# Tells me when the bot logs in.
 @bot.event
 async def on_ready():
     print('Bot logged in.')
 
+# Controls the bot's fetch command
 @bot.command()
 async def fetch(ctx, arg):
     if arg == 'latest':
@@ -17,11 +20,12 @@ async def fetch(ctx, arg):
     else:
         await ctx.send('https://xkcd.com/%s' % str(arg))
 
+#Controls the bot's find command
 @bot.command()
 async def find(ctx, *, arg):
     await ctx.send('`Searching for xkcd comic...`')
     for query in search(arg, tld='com', num=1, stop=1, pause=2):
         await ctx.send(query)
 
-
+# Actually runs the bot, using the key from line 6 as an argument
 bot.run(key)
