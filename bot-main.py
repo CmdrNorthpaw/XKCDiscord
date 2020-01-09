7294242407998f80e8bc5753e145a9a3c4e5f2e0
CmdrNorthpaw <thetuxkid@gmail.com>
2020-01-09 12:24:36 +0000

    Did more work on the reaction function

--- bot-main.py
+++ bot-main.py
@@ -8,6 +8,9 @@ key = environ.get('discordKey')
 
 bot = commands.Bot(command_prefix='xkcd ')
 
+searchList = []
+searchListIndice = 0
+search = ''
 # Tells me when the bot logs in.
 @bot.event
 async def on_ready():
@@ -41,10 +44,15 @@ async def on_reaction_add(reaction, user):
     print(user.name)
     if reaction.emoji == '❎' and user.name != 'XKCDiscord':
         searchListIndice = searchListIndice + 1
-        search = searchList[searchListIndice]
-        search = str(search['url'])
-        search = search.replace('www.', '')
-        await ctx.send(search)
+        if searchListIndice > 10:
+            print("Sorry, that's all the comics I have! Try a different search term.")
+        else:
+            search = searchList[searchListIndice]
+            search = str(search['url'])
+            search = search.replace('www.', '')
+            await ctx.send(search)
+            await ctx.send('Wrong comic? React with ❎ to tell me!')
+
 
 # Actually runs the bot, using the key from line 6 as an argument
 bot.run(key)
